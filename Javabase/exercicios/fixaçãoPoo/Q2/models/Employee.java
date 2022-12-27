c2906876f795abbf9541363ffbdda0da0e7495d0
josceno <jdamasceno414@gmail.com>
2022-12-27 13:42:06 -0300

    q2

--- Javabase/exercicios/fixaçãoPoo/Q2/models/Employee.java
+++ Javabase/exercicios/fixaçãoPoo/Q2/models/Employee.java
@@ -2,13 +2,32 @@ package Javabase.exercicios.fixaçãoPoo.Q2.models;
 
 
 public class Employee {
-    public String nome;
+    public String name;
     public double grossSalary;
     public double tax;
+    
+    public Employee(String name, double grossSalary, double tax ){
+        this.name = name;
+        this.grossSalary = grossSalary;
+        this.tax = tax;
+    }
 
     public double netSalary(){
         return grossSalary - tax;
     }
+    
+    public void icreaseSalary(double perctage){
+        grossSalary = grossSalary + (grossSalary - (grossSalary*perctage/100));
 
+        System.out.println("Updated data "+ name + "R$: " +netSalary() );
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: "
+        +name 
+        + " $ "
+        +netSalary();
+    }
     
 }
