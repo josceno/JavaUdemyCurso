4a32703bfd484c160860358b68afe6bf7307fbd0
josceno <jdamasceno414@gmail.com>
2023-01-15 15:18:45 -0300

    conclusão exercico de arquivos

--- Javabase/exercicios/manipularArquivos/application/Application.java
+++ Javabase/exercicios/manipularArquivos/application/Application.java
@@ -1,13 +1,17 @@
 package Javabase.exercicios.manipularArquivos.application;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import javax.sound.sampled.Line;
 
 import Javabase.exercicios.manipularArquivos.entities.Item;
 
@@ -20,15 +24,27 @@ public class Application {
 
         boolean success = new File(path + "\\out").mkdir();
         List<Item> items = new ArrayList<>(){};
-        String[] entrada = input.nextLine().split(","); 
-        String nome = entrada[0];
-        Double valorUnitario = Double.parseDouble(entrada[1]);
-        Integer quantidade = Integer.parseInt(entrada[2]);
+        String[] entrada = null;
+        try{
+            System.out.println("insira Itens na seginte estrutura:");
+            System.out.println("nome,Valor unitário,quantidade");
+            do{
+                entrada = input.nextLine().split(","); 
+                String nome = entrada[0];
+                Double valorUnitario = Double.parseDouble(entrada[1]);
+                Integer quantidade = Integer.parseInt(entrada[2]);
+                items.add(new Item(nome, valorUnitario, quantidade));
+            }while(!entrada.equals("0"));
+        }catch(ArrayIndexOutOfBoundsException e){
+            if(entrada[0].equals("0")){
+                System.out.println("encerrando o programa, prepardo para exibir...");
+            }else{
+                throw new ArrayIndexOutOfBoundsException("formato de invalido\n o item deve ser inserido no seguinte formato:\n"+
+                "nome,Valor unitário,quantidade");
+            }
 
-        items.add(new Item(nome, valorUnitario, quantidade));
-        items.add(new Item("leite", 5.50, 7));
-        items.add(new Item("manteiga", 1.00, 3));
-
+           
+        }
         
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(path+"\\out\\sumary.csv"))){
             bw.write("nome do item,ValorTotal");
@@ -41,5 +57,14 @@ public class Application {
         }catch(IOException e){
             System.out.println(e.getMessage());
         }
+        System.out.println();
+        try(BufferedReader br = new BufferedReader(new FileReader(path+"\\out\\sumary.csv"))){
+            String line = br.readLine();
+            while(line!=null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }catch(IOException e){}
+        input.close();
     }
 }
