cec4be0f20ae4dc2b97ed23d628743b9f1c871d2
josceno <jdamasceno414@gmail.com>
2023-01-31 13:17:56 -0300

    list.sort comparable

---
+++ Javabase/Poo/Aula253/application/MyComparator.java
@@ -0,0 +1,17 @@
+package Javabase.Poo.Aula253.application;
+
+import java.util.Comparator;
+
+import Javabase.Poo.Aula253.entities.Product;
+
+
+
+public class MyComparator implements Comparator<Product> {
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        
+        return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+    }
+    
+}
---
+++ Javabase/Poo/Aula253/application/Program.java
@@ -0,0 +1,28 @@
+package Javabase.Poo.Aula253.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import Javabase.Poo.Aula253.entities.Product;
+import Javabase.Poo.Aula253.application.MyComparator;
+
+public class Program {
+    
+    
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        
+        list.add(new Product("TV",900.00));
+        list.add(new Product("Tablet",900.00));
+        list.add(new Product("Notebook",900.00));
+        
+        list.sort(new MyComparator());
+        
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
+    
+}
