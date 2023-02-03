eeed664dce5474b1398578039a59ada6d47ea9d6
josceno <jdamasceno414@gmail.com>
2023-02-03 15:13:48 -0300

    exercicio lambda

--- Javabase/exercicios/exerciciofuncional2/application/Arquivos.java
+++ Javabase/exercicios/exerciciofuncional2/application/Arquivos.java
@@ -15,15 +15,23 @@ import Javabase.exercicios.exerciciofuncional2.entites.Funcionário;
 public class Arquivos {
     public static String gerarCaminhoArquivo(){
         String path = "C:\\temp\\funcionarios.csv";
-        List<Funcionário> funcionarios = Arrays.asList(new Funcionário("Moão", "jdamasceno414@gmail.com",1197.90));
+        List<Funcionário> funcionarios = Arrays.asList(
+            new Funcionário("Moão", "jdamasceno414@gmail.com",1197.90),
+            new Funcionário("Noão", "Adamasceno414@gmail.com",1197.90),
+            new Funcionário("Maão", "Bdamasceno414@gmail.com",1197.90)
+            
+            );
         
-        funcionarios.forEach(x -> {
+       
             try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){  
-                bw.write(x.toString()); 
-                bw.newLine();
+               for (Funcionário funcionário : funcionarios) {
+                    bw.write(funcionário.toString()); 
+                    bw.newLine();
+               }
+               
             }catch(IOException e){
                 System.out.println(e.getMessage());
-        }});
+            }
         return path; 
     }
 
@@ -44,7 +52,10 @@ public class Arquivos {
         double sum = funcionários.stream()
             .filter(x -> x.getNome().charAt(0) == 'M')
             .map(x -> x.getSalario()).reduce(0.0,(x,y)-> x+y);
-
+       
+       funcionários.sort((f1,f2)-> f1.getEmail().compareTo(f2.getEmail()));
+       funcionários.forEach(x -> System.out.println(x));
+      
        System.out.println(sum);     
     }
 }
